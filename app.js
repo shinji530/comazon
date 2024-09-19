f0f54e479463e341431b5399931ad34d306fac2e
shinji530 <shinji53059@gmail.com>
2024-09-19 18:25:46 +0900

    Feat: order 추가

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { assert, create } from 'superstruct';
+import { CreateUser, PatchUser, CreateProduct, PatchProduct, CreateOrder, PatchOrder } from './structs.js';
 
 const prisma = new PrismaClient();
 
@@ -13,12 +15,8 @@ function asyncHandler(handler) {
     try {
       await handler(req, res);
     } catch (e) {
-      console.log(e.name);
-      console.log(e.message);
-      if (e.name === "ValidationError") {
+      if (e.name === "StructError" || e instanceof Prisma.PrismaClientValidationError) {
         res.status(400).send({ message: e.message });
-      } else if (e.name === "CastError") {
-        res.status(404).send({ message: "Cannot find given id." });
       } else {
         res.status(500).send({ message: e.message });
       }
@@ -31,7 +29,33 @@ function asyncHandler(handler) {
 /*********** users ***********/
 
 app.get('/users', asyncHandler( async (req, res) => {
-  const users = await prisma.user.findMany();
+  // destructuring query string
+  const { offset = 0, limit = 10, order = 'newest' } = req.query;
+  // req.query에 offset이라는 속성이 아예 없다면 offset 변수에 0이 들어감
+  // 하지만 req.query에 offset이라는 속성이 있다면 그 값이 문자열로 들어감
+  // 날짜 오름차순 : 과거부터 현재로 oldest
+  let orderBy;
+  switch (order) {
+    case 'oldest':
+      orderBy = { createdAt: 'asc' };
+      break;
+    case 'newest':
+    default:
+      orderBy = { createdAt: 'desc' };
+  }
+
+  const users = await prisma.user.findMany({
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit),
+    include: {
+      userPreference: {
+        select: {
+          receiveEmail: true,
+        }
+      }
+    }
+  });
   res.send(users);
 }));
 
@@ -39,24 +63,47 @@ app.get('/users/:id', asyncHandler( async (req, res) => {
   const { id } = req.params;
   const user = await prisma.user.findUnique({
     where: { id },
+    include: {
+      userPreference: true,
+    }
   });
   res.send(user);
 }));
 
 app.post('/users', asyncHandler( async (req, res) => {
+  assert(req.body, CreateUser);
   // 리퀘스트 바디 내용으로 유저 생성
+  const { userPreference, ...userFields } = req.body;
   const user = await prisma.user.create({
-    data: req.body,
+    data: {
+      ...userFields,
+      userPreference: {
+        create: userPreference,
+      },
+    },
+    include: {
+      userPreference: true,
+    }
   });
   res.status(201).send(user);
 }));
 
 app.patch('/users/:id', asyncHandler( async (req, res) => {
+  assert(req.body, PatchUser);
   const { id } = req.params;
+  const { userPreference, ...userFields } = req.body;
   // 리퀘스트 바디 내용으로 id에 해당하는 유저 수정
   const user = await prisma.user.update({
     where: { id },
-    data: req.body,
+    data: {
+      ...userFields,
+      userPreference: {
+        update: userPreference,
+      }
+    },
+    include: {
+      userPreference: true,
+    }
   })
   res.send(user);
 }));
@@ -70,10 +117,55 @@ app.delete('/users/:id', asyncHandler( async (req, res) => {
   res.sendStatus(204);
 }));
 
+app.get('/users/:id/saved-products', asyncHandler( async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { id },
+    include: {
+      savedProducts: true,
+    }
+  });
+  res.send(user.savedProducts);
+}));
+
+app.get('/users/:id/orders', asyncHandler( async (req, res) => {
+  const { id } = req.params;
+  const { orders } = await prisma.user.findUniqueOrThrow({
+    where: { id },
+    include: {
+      orders: true,
+    }
+  });
+  res.send(orders);
+}));
+
 /*********** products ***********/
 
 app.get('/products', asyncHandler( async (req, res) => {
-  const products = await prisma.product.findMany();
+  const { offset = 0, limit = 10, order = 'newest', category } = req.query;
+  let orderBy;
+  switch (order) {
+    case 'priceLowest':
+      orderBy = { price: 'asc' };
+      break;
+    case 'priceHighest':
+      orderBy = { price: 'desc' };
+      break;
+    case 'oldest':
+      orderBy = { createdAt: 'asc' };
+      break;
+    case 'newest':
+    default:
+      orderBy = { createdAt: 'desc' };
+  }
+  const where = category ? { category } : {};
+  const options = {
+    orderBy,
+    where,
+    skip: parseInt(offset),
+    take: parseInt(limit),
+  }
+  const products = await prisma.product.findMany(options);
   res.send(products);
 }));
 
@@ -86,6 +178,7 @@ app.get('/products/:id', asyncHandler( async (req, res) => {
 }));
 
 app.post('/products', asyncHandler( async (req, res) => {
+  assert(req.body, CreateProduct);
   const product = await prisma.product.create({
     data: req.body,
   });
@@ -93,6 +186,7 @@ app.post('/products', asyncHandler( async (req, res) => {
 }));
 
 app.patch('/products/:id', asyncHandler( async (req, res) => {
+  assert(req.body, PatchProduct);
   const { id } = req.params;
   const product = await prisma.product.update({
     where: { id },
@@ -108,5 +202,65 @@ app.delete('/products/:id', asyncHandler( async (req, res) => {
   });
   res.sendStatus(204);
 }));
+
+/*********** orders ***********/
+
+app.get('/orders', asyncHandler(async (req, res) => {
+  const orders = await prisma.order.findMany();
+  res.send(orders);
+}));
+
+app.get('/orders/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const order = await prisma.order.findUniqueOrThrow({
+    where: { id },
+    include: {
+      orderItems: true,
+    }
+  });
+  const total = order.orderItems.reduce((acc, orderItem) => {
+    return acc += orderItem.unitPrice * orderItem.quantity;
+  }, 0);
+  // let total = 0;
+  // order.orderItems.forEach((orderItem) => {
+  //   total += orderItem.unitPrice * orderItem.quantity;
+  // });
+  order.total = total;
+  res.send(order);
+}));
+
+app.post('/orders', asyncHandler(async (req, res) => {
+  assert(req.body, CreateOrder);
+  const { orderItems, ...orderFields } = req.body;
+  const order = await prisma.order.create({
+    data: {
+      ...orderFields,
+      orderItems: {
+        create: orderItems,
+      },
+    },
+    include: {
+      orderItems: true,
+    },
+  });
+  res.status(201).send(order);
+}));
+
+app.patch('/orders/:id', asyncHandler(async (req, res) => {
+  assert(req.body, PatchOrder);
+  const { id } = req.params;
+  const order = await prisma.order.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(order);
+}));
+
+app.delete('/orders/:id', asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await prisma.order.delete({ where: { id } });
+  res.sendStatus(204);
+}));
+
 
 app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
---
+++ prisma/migrations/20240919054532_add_user_order_relation/migration.sql
@@ -0,0 +1,37 @@
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'COMPLETE');
+
+-- CreateTable
+CREATE TABLE "UserPreference" (
+    "id" TEXT NOT NULL,
+    "receiveEmail" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserPreference_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "status" "OrderStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL,
+    "unitPrice" DOUBLE PRECISION NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
